19a83534e79f9c4d59780bb1106e47d4b183fb64
Vítor Barroso <accounts@owozsh.dev>
2024-04-13 11:57:26 -0300

    Add devenv config

---
+++ devenv.nix
@@ -0,0 +1,48 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.GREET = "devenv";
+
+  packages = [
+    pkgs.git
+    pkgs.elixir_1_16
+    pkgs.erlang_26
+    pkgs.postgresql_16
+    pkgs.inotify-tools
+  ];
+
+  scripts.hello.exec = ''
+    echo
+    echo "Starting blogx development environment"
+    echo
+  '';
+
+  scripts.dev.exec = ''
+    if ! psql -l | grep -q "blog-db"; then mix setup; fi
+    mix phx.server
+  '';
+
+  scripts.c.exec = ''
+    iex -S mix
+  '';
+
+  enterShell = ''
+    hello
+    elixir -v
+  '';
+
+  services.postgres = {
+    enable = true;
+    port = 5500;
+    package = pkgs.postgresql_16;
+    initialScript = ''
+      CREATE USER postgres WITH PASSWORD 'postgres' SUPERUSER;
+    '';
+  };
+
+  enterTest = ''
+    echo "Running tests"
+    elixir -v | grep "26"
+    elixir -v | grep "1.16"
+  '';
+}
